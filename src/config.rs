6b35938f7b18bad844644d423fb2682c26a45cf5
gmg <gmg137@live.com>
2022-08-22 13:24:20 +0800

    发布 2.0.2 版本

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "2.0.0";
+pub static VERSION: &str = "2.0.2";
 pub static GETTEXT_PACKAGE: &str = "netease-cloud-music-gtk4";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/netease-cloud-music-gtk4";
