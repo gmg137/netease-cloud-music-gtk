847f2cc5eae03d5e5aaae2ad7a77d242135a26b0
catsout <outline941@live.com>
2022-10-28 08:45:14 +0800

    Get CARGO_HOME from env first

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -3,8 +3,7 @@
 export MESON_BUILD_ROOT="$1"
 export MESON_SOURCE_ROOT="$2"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
-export CARGO_HOME="$MESON_BUILD_ROOT"/cargo-home
-export CARGO_HOME="$HOME"/.cargo
+export CARGO_HOME=${CARGO_HOME:="$HOME/.cargo"}
 export OUTPUT="$3"
 export BUILDTYPE="$4"
 export APP_BIN="$5"
