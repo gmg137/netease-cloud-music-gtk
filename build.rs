310540061a274bcc1a1f987c8ac13340ee91439e
rongbo <wurongbo2012@hotmail.com>
2025-09-22 17:50:36 +0800

    rebuild appimage and macos.app

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
 	    env::set_var("GST_PLUGIN_PATH", lib);
 	    env::set_var("DYLD_FALLBACK_LIBRARY_PATH", lib);
 	}
+        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=11.0");
         println!("cargo:rustc-link-search=framework=/Library/Frameworks");
         println!("cargo:rustc-link-arg=-Wl,-headerpad_max_install_names,-rpath,/Library/Frameworks/GStreamer.framework/Libraries");
     }
