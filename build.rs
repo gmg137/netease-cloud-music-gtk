8b9acd125409b448de487478558333be483d1528
rongbo <wurongbo2012@hotmail.com>
2025-09-22 15:04:43 +0800

    build with macos 13 x86_64

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::env;
+fn main() {
+    let target = env::var("CARGO_CFG_TARGET_OS");
+    if target == Ok("macos".to_string()) {
+        unsafe {
+	    env::set_var("PKG_CONFIG_PATH",
+	        "/usr/local/lib/pkgconfig:/Library/Frameworks/GStreamer.framework/Libraries/pkgconfig"
+	    );
+	    let lib = "/Library/Frameworks/GStreamer.framework/Libraries";
+	    env::set_var("GST_PLUGIN_PATH", lib);
+	    env::set_var("DYLD_FALLBACK_LIBRARY_PATH", lib);
+	}
+        println!("cargo:rustc-link-search=framework=/Library/Frameworks");
+        println!("cargo:rustc-link-arg=-Wl,-headerpad_max_install_names,-rpath,/Library/Frameworks/GStreamer.framework/Libraries");
+    }
+}
